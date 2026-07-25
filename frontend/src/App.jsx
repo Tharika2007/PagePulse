1c4c2a7cc3c633e19e54cc6e9550ae7eec48e590
Tharika <tharikak2007@gmail.com>
2026-07-25 23:17:13 +0530

    Connected frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/audit",
+        "https://pagepulse-1-1f95.onrender.com",
         {
           url: url,
         }
